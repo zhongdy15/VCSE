0017f5ab07dee7fabcfcd5bd444c0fa1538144dd
zhongdy15 <zhongdy15@foxmail.com>
2024-03-14 23:52:22 +0800

    跑所有的minigrid实验

--- VCSE_A2C/rl-starter-files/rl-starter-files/run_original.py
+++ VCSE_A2C/rl-starter-files/rl-starter-files/run_original.py
@@ -1,9 +1,11 @@
 import os
 import time
-gpu_id_list = [3,3,3,3]
-for task in {'SimpleCrossingS9N1'}:
+time_flag = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+gpu_id_list = [4,4,4,4]
+task_set = {"LavaGapS7", "Empty-16x16", "DoorKey-6x6", "DoorKey-8x8", "Unlock"}
+for task in task_set:#{'SimpleCrossingS9N1'}:
     for seed in {1,2,3,4}:
-        cmd_line = f"CUDA_VISIBLE_DEVICES={gpu_id_list[seed]} python3 -m scripts.train --algo a2c --env MiniGrid-{task}-v0 --model {task}/MiniGrid-{task}-v0-original-{seed} " \
+        cmd_line = f"CUDA_VISIBLE_DEVICES={gpu_id_list[seed-1]} python3 -m scripts.train --algo a2c --env MiniGrid-{task}-v0 --model {task+'_'+time_flag}/MiniGrid-{task}-v0-original-{seed} " \
                    f"--save-interval 100 --frames 1000000 --seed {seed} --use_batch"
         print(cmd_line)
         os.system(cmd_line)
--- VCSE_A2C/rl-starter-files/rl-starter-files/run_vcse.py
+++ VCSE_A2C/rl-starter-files/rl-starter-files/run_vcse.py
@@ -2,9 +2,11 @@ import os
 import time
 gpu_id = 1
 time_flag = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
-for task in {'SimpleCrossingS9N1'}:
+
+task_set = {"LavaGapS7", "Empty-16x16", "DoorKey-6x6", "DoorKey-8x8", "Unlock"}
+for task in task_set:#{'SimpleCrossingS9N1'}:
     for seed in {1,2,3,4}:
-        cmd_line = f"CUDA_VISIBLE_DEVICES={gpu_id} python3 -m scripts.train --algo a2c --env MiniGrid-{task}-v0 --model {task}/MiniGrid-{task}-v0-vcse-{seed} " \
+        cmd_line = f"CUDA_VISIBLE_DEVICES={gpu_id} python3 -m scripts.train --algo a2c --env MiniGrid-{task}-v0 --model {task+'_'+time_flag}/MiniGrid-{task}-v0-vcse-{seed} " \
                    f"--save-interval 100 --frames 1000000 --use_entropy_reward --use_value_condition --seed {seed} --beta 0.005 --use_batch"
         print(cmd_line)
         os.system(cmd_line)
