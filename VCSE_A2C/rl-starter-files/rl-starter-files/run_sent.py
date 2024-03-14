945bd3fbdffeffd50e2cffb655d71fd084f079e8
zhongdy15 <zhongdy15@foxmail.com>
2024-03-14 17:18:04 +0800

    添加time_flag

--- VCSE_A2C/rl-starter-files/rl-starter-files/run_sent.py
+++ VCSE_A2C/rl-starter-files/rl-starter-files/run_sent.py
@@ -1,9 +1,11 @@
 import os
 import time
 gpu_id_list = [4,4,4,4]
+time_flag = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+
 for task in {'SimpleCrossingS9N1'}:
     for seed in {1,2,3,4}:
-        cmd_line = f"CUDA_VISIBLE_DEVICES={gpu_id_list[seed]} python3 -m scripts.train --algo a2c --env MiniGrid-{task}-v0 --model {task}/MiniGrid-{task}-v0-sent-{seed} " \
+        cmd_line = f"CUDA_VISIBLE_DEVICES={gpu_id_list[seed]} python3 -m scripts.train --algo a2c --env MiniGrid-{task}-v0 --model {task+'_'+time_flag}/MiniGrid-{task}-v0-sent-{seed} " \
                    f"--save-interval 100 --frames 1000000 --use_entropy_reward --seed {seed} --beta 0.005"
         print(cmd_line)
         os.system(cmd_line)
