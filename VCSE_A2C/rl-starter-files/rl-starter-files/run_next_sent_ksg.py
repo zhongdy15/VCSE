07c8f381379a5267d95e4c0495b860061e8c3102
zhongdy15 <zhongdy15@foxmail.com>
2024-03-20 11:27:52 +0800

    Update run_next_sent_ksg.py

--- VCSE_A2C/rl-starter-files/rl-starter-files/run_next_sent_ksg.py
+++ VCSE_A2C/rl-starter-files/rl-starter-files/run_next_sent_ksg.py
@@ -1,12 +1,12 @@
 import os
 import time
-gpu_id_list = [7,7,7,7]
+gpu_id_list = 6
 time_flag = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
 task_set = {"LavaGapS7", "Empty-16x16", "DoorKey-6x6", "DoorKey-8x8", "Unlock"}
-task_set = {"DoorKey-8x8"}
+task_set = {"SimpleCrossingS9N1"}
 for task in task_set:#{'SimpleCrossingS9N1'}:
     for seed in {1,2,3,4}:
-        cmd_line = f"CUDA_VISIBLE_DEVICES={gpu_id_list[seed-1]} python3 -m scripts.train --algo a2c --env MiniGrid-{task}-v0 --model {task+'_'+time_flag}/MiniGrid-{task}-v0-nextsentksg-{seed} " \
+        cmd_line = f"CUDA_VISIBLE_DEVICES={gpu_id_list} python3 -m scripts.train --algo a2c --env MiniGrid-{task}-v0 --model {task+'_'+time_flag}/MiniGrid-{task}-v0-nextsentksg-{seed} " \
                    f"--save-interval 100 --frames 3000000 --use_entropy_reward --use_nextstate_entropy_reward --use_ksg --seed {seed} --beta 0.005"
         print(cmd_line)
         os.system(cmd_line)
